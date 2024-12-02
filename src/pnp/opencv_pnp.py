6d1dc403bc030793fc63518f242164c83d69de79
Daniel Dai <daniel.dai.email@gmail.com>
2024-12-02 11:04:11 -0500

    fixed pnp_flag for opencv pnp

--- src/pnp/opencv_pnp.py
+++ src/pnp/opencv_pnp.py
@@ -2,10 +2,11 @@ from pnp import PNP
 import cv2
 
 class OpenCVPNP(PNP):
-    def __init__(self, camera_config):
+    def __init__(self, camera_config, pnp_flag=cv2.SOLVEPNP_ITERATIVE):
         super().__init__(camera_config)
         self.use_opencv = True
+        self.pnp_flag = pnp_flag
 
     def estimate_pose(self, object_points, image_points):
-        success, rvec, tvec, _ = cv2.solvePnP(object_points, image_points, self.camera_config.camera_matrix, None, flags=cv2.SOLVEPNP_ITERATIVE)
+        success, rvec, tvec, _ = cv2.solvePnP(object_points, image_points, self.camera_config.camera_matrix, None, flags=self.pnp_flag)
         return success, rvec, tvec
